da331176f389d76dd906322567c7b3d9a95124d9
snork-maiden <pollykoldysheva@gmail.com>
2024-06-15 16:07:00 +0300

    Benefits component, benefits icon

---
+++ components/Benefits/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2 class="title">
+    Плюсы использования сервиса «Компас» для анализа штрафов
+  </h2>
+</template>
+<style scoped lang="scss">
+@import "~/assets/scss/variables.scss";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,5 +9,6 @@ useSeoMeta({
   <main class="main">
     <h1 class="visually-hidden">Сервис для проверки штрафов Компас</h1>
     <PenaltyCheck/>
+    <Benefits/>
   </main>
 </template>
