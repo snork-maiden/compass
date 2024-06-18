68d965c8942c2bbedde3cdfa134e0e594b2db168
snork-maiden <pollykoldysheva@gmail.com>
2024-06-18 13:47:35 +0300

    Change favicon. embed youtube video

---
+++ components/Modal.vue
@@ -0,0 +1,52 @@
+<script setup>
+defineEmits({
+  close: null,
+});
+</script>
+
+<template>
+  <Teleport to="body">
+    <div @click.self="$emit('close')" class="modal-overlay">
+      <div class="modal-container">
+        <button @click="$emit('close')" class="close-button">
+          <IconClose />
+        </button>
+        <slot></slot>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<style scoped lang="scss">
+@import "~/assets/scss/variables.scss";
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.79);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.modal-container {
+  position: relative;
+  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.1), 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.close-button {
+  position: absolute;
+  top: -25px;
+  right: 0;
+  cursor: pointer;
+  background: none;
+  border: 0;
+  &:hover {
+    transform: scale(1.05);
+  }
+}
+</style>
---
+++ components/PenaltyCheck/Form/Video.vue
@@ -0,0 +1,43 @@
+<script setup>
+let isModalOpen = ref(false);
+</script>
+
+<template>
+  <button type="button" class="show-video" @click="isModalOpen = true">
+    <IconVideo />
+    <span class="btn-text">О сервисе</span>
+    <span class="duration">(1 мин. 20 сек)</span>
+  </button>
+
+  <Modal v-if="isModalOpen" @close="isModalOpen = false">
+    <iframe
+      width="960"
+      height="523"
+      src="https://www.youtube.com/embed/yKQ_sQKBASM?si=V7fnab2-4BQ6GIKv"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+  </Modal>
+</template>
+<style scoped lang="scss">
+@import "~/assets/scss/variables.scss";
+
+.show-video {
+  cursor: pointer;
+  background: none;
+  border: 1px solid $button-bg-color;
+  display: flex;
+  align-items: center;
+  gap: 0.3em;
+  padding: 0.55em 1em;
+  border-radius: 5px;
+}
+
+.duration {
+  color: $accent-text-color;
+  font-size: 15px;
+}
+</style>
